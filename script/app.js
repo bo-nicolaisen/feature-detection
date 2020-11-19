904f6049d86af5c74c2247cf926da4876567fe9a
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2020-11-19 07:51:29 +0100

    geo location
    check for geo location

---
+++ script/app.js
@@ -0,0 +1,68 @@
+
+
+featureCheck();
+
+
+// controller
+function featureCheck() {
+
+  let geoloc = checkGeolocationFeature();
+
+  if (geoloc) {
+
+    navigator.geolocation.getCurrentPosition(showLocation);
+
+
+  }
+  else {
+
+    alternativeView();
+
+  }
+
+}
+
+
+checkGeolocationFeature();
+
+
+function checkGeolocationFeature() {
+
+  if ("geolocation" in navigator) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+
+
+//wiev
+
+
+function showLocation(myPos) {
+
+
+  let crd = myPos.coords;
+
+  document.getElementById("app").innerHTML = "yay geolocation <br>" +
+
+    "Du er her:<br>" +
+    `breddegrad : ${crd.latitude}<br>` +
+    `længdegrad: ${crd.longitude}<br>` +
+    `inden for ca ${crd.accuracy} meter.`;
+
+
+
+}
+
+
+function alternativeView(){
+  document.getElementById("app").innerHTML = "no geo location service";
+
+}
+
+
+
+
